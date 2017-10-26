5e711a3e3f1df3a997537498ff2484ece067cc11
Antoine Lecoutre <Antoine-Lecoutre@outlook.com>
2017-10-26 17:07:37 +0200

    Mise en place des commentaires Mise en place de la responsivité Mise en place des zones de jouabilité Mise en place du fichier php permetant la génération des lettres aléatoire

--- js/main.js
+++ js/main.js
@@ -1,29 +1,53 @@
+/**
+ * La variable i nous permet de comptabiliser le tirage des lettres
+ * @type {number}
+ */
 var i = 1;
 
 (function () {
-
-    //var consomne = ["z","r","t","p","q","s","d","f","g","h","j","k","k","l","m","w","x","c","v","b","n"];
-    //var voyelle = ["a","e","y","u","i","o"];
-
+    /**
+     * La variable j d'ajouter une lettre a notre mots réponse
+     * @type {number}
+     */
     var j=1;
 
+    /**
+     * Clique du bouton Jouer
+     */
     $(".playButton").click(function(){
+        /**
+         * Désactive le button Play
+         * @type {boolean}
+         */
         this.disabled = true;
+        /**
+         * Fait apparaitre les boutons Consomne et Voyelle
+         */
         $(".btnConsomne").show("slow");
         $(".btnVoyelle").show("slow");
-
     });
 
+    /**
+     * Clique du bouton Consomne
+     */
     $(".btnConsomne").click(function () {
-        //$("#Lettre" + i).html(consomne[getRandomInt(0, consomne.length)]);
+        /**
+         * Permet de récupérer une consomne depuis le fichier lettre.php
+         */
         $.get('/lettre.php?type=consomne', function (e) {
             $("#Lettre" + i).html(e);
             $("#Lettre" + i).show("slow");
             finPreparation();
         });
     });
+
+    /**
+     * Clique du bouton Voyelle
+     */
     $(".btnVoyelle").click(function () {
-        //$("#Lettre" + i).html(voyelle[getRandomInt(0, voyelle.length)]);
+        /**
+         * Permet de récupérer une voyelle depuis le fichier lettre.php
+         */
         $.get('/lettre.php?type=voyelle', function (e) {
             $("#Lettre" + i).html(e);
             $("#Lettre" + i).show("slow");
@@ -31,25 +55,54 @@ var i = 1;
         });
     });
 
+    /**
+     * Lorsque l'on clique sur une des lettres proposer
+     */
     $(".Lettre").click(function () {
+        /**
+         * Faire apparaitre la lettre cliqué en dessous pour former le mot
+         */
         $("#Reponse" + j).show("slow").html(this.textContent);
+
+        /**
+         * Desactive la lettre cliquer
+         */
         this.disabled = true;
         j++;
 
+        /**
+         * Active le bouton reset
+         */
         $(".btnResetLettre").removeAttr("disabled");
     });
 
+    /**
+     * Lorsque l'on clique sur le bouton reset
+     */
     $(".btnResetLettre").click(function () {
+
+        /**
+         * Desactive le bouton reset
+         */
         $(".btnResetLettre").attr('disabled', true);
+
+        /**
+         * Reset le mot composé et déverouille les lettres cliquées avant
+         */
         for(blbl = 1; blbl < 10; blbl++){
             $("#Lettre" + blbl).removeAttr("disabled");
             $("#Reponse" + blbl).hide("slow");
         }
-        i=1;
         j=1;
     });
 
+    /**
+     * Permet de rejouer et meme refresh la page
+     */
     $(".F5").click(function () {
+        /**
+         * Desactive tout les boutons
+         */
         for(blbl = 1; blbl < 10; blbl++){
             $("#Reponse" + blbl).hide("slow");
             $("#Lettre" + blbl).hide("slow");
@@ -58,31 +111,30 @@ var i = 1;
         $(".btnConsomne").hide("slow");
         $(".btnVoyelle").hide("slow");
 
+        /**
+         * Reload la page au bout de 0.7 seconde
+         */
         setTimeout("location.reload()",700);
     });
 }());
 
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min;
-}
-
-function getVoyelle() {
-    $.get('/lettre.php?type=voyelle', function (e) {
-        return e
-    });
-}
-
-function getConsomne() {
-    $.get('/lettre.php?type=consomne', function (e) {
-        return e
-    });
-}
-
+/**
+ * Cette fonctione permet d'increment i et de vérifier si le nombre de lettre est inférieur a 10
+ */
 function finPreparation() {
+    /**
+     * Incrémentation de i
+     */
     i++;
+
+    /**
+     * Dévérouille le bouton F5
+     */
     $(".F5").removeAttr("disabled");
+
+    /**
+     * Vérification du nombre de lettre
+     */
     if(i>9){
         $(".btnConsomne").attr('disabled', true);
         $(".btnVoyelle").attr('disabled', true);
---
+++ lettre.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Antoi
+ * Date: 26/10/2017
+ * Time: 13:57
+ */
+
+// Déclaration des différentes variables utiles à la connexion
+$serveur = "localhost"; $base = 'desChiffresEtDesLettres'; $utilisateur = 'root'; $motDePasse = 'root';
+
+// création d'une connexion
+try {
+    $dns = "mysql:host=$serveur;dbname=$base";
+    $conn = new PDO( $dns, $utilisateur, $motDePasse );
+} catch ( Exception $e ) {
+    echo "Connexion MySQL impossible : ", $e->getMessage();
+    die();
+}
+
+$value = "!";
+$tmp = "";
+
+/**
+ * Génération aléatoire d'une consomne ou bien d'une voyelle
+ */
+if($_GET['type'] == "voyelle"){
+
+    $voyelle = array("a","e","y","u","i","o");
+    $value =  $voyelle[rand(0, count($voyelle)-1)];
+
+}elseif ($_GET['type'] == "consomne"){
+
+    $consomne = array("z","r","t","p","q","s","d","f","g","h","j","k","k","l","m","w","x","c","v","b","n");
+    $value =  $consomne[rand(0, count($consomne)-1)];
+}
+
+/**
+ * Envoie des nouvelles stats
+ */
+//$sql = $conn->prepare('UPDATE table SET nbAppel = '.$tmp.' WHERE lettre = "'.$value.'";');
+$sql = $conn->prepare('UPDATE lettre SET nbAppel = nbAppel+1 WHERE lettre = :lettre ;');
+
+try {
+    // Préparation des données
+    $donnees = array(
+        'lettre'=>$value
+    );
+
+    // Envoi de la requête avec les données
+    $success = $sql->execute($donnees);
+
+} catch( Exception $e ){
+    echo 'Erreur de requète : ', $e->getMessage();
+}
+
+/**
+ * Envoie de la valeur dans le main.js
+ */
+echo $value;
